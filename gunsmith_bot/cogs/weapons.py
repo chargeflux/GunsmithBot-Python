72c921f4420d3c71c6ffb4dd7e9296fdb69b4b86
chargeflux <20979457+chargeflux@users.noreply.github.com>
2020-03-20 16:34:39 -0400

    Moved weapons to separate cog

---
+++ gunsmith_bot/cogs/weapons.py
@@ -0,0 +1,115 @@
+import logging
+import os
+import asyncio
+from sqlite3 import OperationalError
+import discord
+from discord.ext import commands
+from armory import Armory
+from . import constants
+
+logger = logging.getLogger('Gunsmith.Weapons')
+
+class Weapons(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.group(invoke_without_command=True, 
+            brief="Get information about weapons or perks", 
+            description="Get the rolls for a weapon or information about a perk", 
+            usage="<weapon>",
+            help="")
+    async def gunsmith(self, ctx, *, arg):
+        '''
+        This function corresponds to the "!gunsmith <weapon>" command.
+
+        Parameters
+        ----------
+        ctx
+            The context of the command being invoked. Constructed by `discord.py`
+        arg
+            The arguments of the command, after "!gunsmith"
+        '''
+        weapon = arg
+
+        logger.info(ctx.message.content)
+
+        if len(weapon) < 3:
+            await ctx.send("Please enter a query of 3 or more characters!")
+            return
+
+        if not os.path.exists(self.bot.current_state.current_manifest):
+            logger.critical(f"Manifest queried does not exist at {self.bot.current_state.current_manifest}")
+            await ctx.send("An error occured. Please try again!")
+            return
+
+        armory = Armory(self.bot.current_state.current_manifest)
+
+        logger.info(f"Searching for '{weapon}'")
+        weapons = await armory.get_weapon_details(weapon)
+
+        logger.info(f"# of weapons found: {len(weapons)}")
+        result = weapons[0] # TODO: pagination
+
+        logger.info("Constructing weapon result")
+        DESCRIPTION = str(result.weapon_base_info) + "\n" + result.description
+        embed = discord.Embed(title=result.name, description= DESCRIPTION, color=constants.DISCORD_BG_HEX)
+        embed.set_thumbnail(url=result.icon)
+
+        for perk in result.weapon_perks:
+            if (perk.idx + 1) % 3 == 0:
+                embed.add_field(name="\u200b", value="\u200b", inline=True)
+            embed.add_field(name=perk.name, value=perk, inline=True)
+        embed.add_field(name="\u200b", value="\u200b", inline=True)
+
+        logger.info("Sending weapon result")
+        await ctx.send(embed=embed)
+
+    @gunsmith.command(name="-perk",
+                      brief="Get information about a perk", 
+                      description="Get information about a perk", 
+                      usage="<perk>",
+                      help="")
+    async def perk(self, ctx, *, arg):
+        await ctx.send("Not implemented yet!")
+        return
+
+    @gunsmith.error
+    @perk.error
+    async def on_error(self, ctx, error):
+        if ctx.invoked_subcommand == "-perk":
+            command_type = "perk"
+        else:
+            command_type = "weapon"
+        if hasattr(error, 'original'):
+            logger.exception(error.original)
+            if isinstance(error.original, ValueError):
+                logger.error(f"Command: {ctx.message.content}")
+                await ctx.send(f'{command_type.title()} could not be found.')
+                return
+            if isinstance(error.original, TypeError):
+                logger.error(f"Command: {ctx.message.content}")
+                logger.error(f'Failed to parse {command_type}')
+                await ctx.send(f'Failed to parse {command_type}. Please try again.')
+                return
+            if isinstance(error.original, discord.errors.HTTPException):
+                if error.original.status == 429:
+                    logger.critical("Bot is rate-limited")
+                return
+            if isinstance(error.original, OperationalError):
+                logger.error(f"Command: {ctx.message.content}")
+                logger.error('Failed to find manifest')
+                await ctx.send('An error occured. Please try again.')
+                return
+        if isinstance(error, commands.BadArgument):
+            await ctx.send(f"Please enter the {command_type} name.")
+            return
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send(f"Please enter the {command_type} name. Run '!gunsmith -help' for more information.")
+            return
+
+def setup(bot):
+    bot.add_cog(Weapons(bot))
+
+def teardown(bot):
+    logger.info("Tearing down cogs.weapon & pydest")
+    asyncio.get_event_loop().run_until_complete(bot.current_state.destiny_api.close())
--- gunsmith_bot/gunsmith_bot.py
+++ gunsmith_bot/gunsmith_bot.py
@@ -4,12 +4,10 @@ import os
 import asyncio
 import datetime
 from dataclasses import dataclass, field
-from sqlite3 import OperationalError
 import discord
 from discord.ext import commands, tasks
 import pydest
-import constants
-from armory import Armory, pydest_loader
+from armory import pydest_loader
 
 if not os.path.exists("logs/"):
     os.mkdir("logs")
@@ -38,7 +36,7 @@ class State():
     old_manifests: [str] = field(default_factory=list)
 
 bot = commands.Bot(command_prefix="!", description='Retrieve rolls for Destiny 2 weapons')
-current_state: State = State()
+bot.current_state: State = State()
 
 class UpdateManifest(commands.Cog):
     def __init__(self, bot):
@@ -50,21 +48,20 @@ class UpdateManifest(commands.Cog):
 
     @tasks.loop(hours=24)
     async def update_manifest(self):
-        for old_manifest in current_state.old_manifests:
+        for old_manifest in bot.current_state.old_manifests:
             try:
                 os.remove(old_manifest)
             except OSError as ex:
                 logger.critical(f"Failed to remove old manifest: {old_manifest}")
                 logger.exception(ex)
-        current_state.old_manifests = [] # note: clears even if deletion is unsucessful
+        bot.current_state.old_manifests = [] # note: clears even if deletion is unsucessful
 
-        if current_state.destiny_api:
-            await pydest_loader.update_manifest(current_state.destiny_api)
-            manifest_location = await pydest_loader.get_manifest(current_state.destiny_api)
-            if manifest_location != current_state.current_manifest:
-                current_state.old_manifests.append(current_state.current_manifest)
-                current_state.current_manifest = manifest_location
-                
+        if bot.current_state.destiny_api:
+            await pydest_loader.update_manifest(bot.current_state.destiny_api)
+            manifest_location = await pydest_loader.get_manifest(bot.current_state.destiny_api)
+            if manifest_location != bot.current_state.current_manifest:
+                bot.current_state.old_manifests.append(bot.current_state.current_manifest)
+                bot.current_state.current_manifest = manifest_location
 
     @update_manifest.before_loop
     async def before_update_manifest(self):
@@ -77,95 +74,20 @@ async def on_ready():
     the start up of the bot.
     """
     logger.info(f'We have logged in as {bot.user}')
-    if not current_state.current_manifest:
+    if not bot.current_state.current_manifest:
         try:
-            current_state.destiny_api = await pydest_loader.initialize_destiny()
-            current_state.current_manifest = await pydest_loader.get_manifest(current_state.destiny_api)
+            bot.current_state.destiny_api = await pydest_loader.initialize_destiny()
+            bot.current_state.current_manifest = await pydest_loader.get_manifest(bot.current_state.destiny_api)
         except pydest.PydestException:
             logger.critical("Failed to initialize PyDest. Quitting.")
             await bot.logout()
         except AttributeError:
             logger.critical("Failed to retrieve manifest. Quitting.")
-            current_state.destiny_api.close()
+            await bot.current_state.destiny_api.close()
             await bot.logout()
-
-@bot.command()
-async def gunsmith(ctx, *, arg):
-    '''
-    This function corresponds to the "!gunsmith <weapon>" command.
-
-    Parameters
-    ----------
-    ctx
-        The context of the command being invoked. Constructed by `discord.py`
-    arg
-        The arguments of the command, after "!gunsmith"
-    '''
-    weapon = arg
-
-    logger.info(ctx.message.content)
-
-    if len(weapon) < 3:
-        await ctx.send("Please enter a query of 3 or more characters!")
-        return
-
-    if not os.path.exists(current_state.current_manifest):
-        logger.critical(f"Manifest queried does not exist at {current_state.current_manifest}")
-        await ctx.send("An error occured. Please try again!")
-        return
-
-    armory = Armory(current_state.current_manifest)
-
-    logger.info(f"Searching for '{weapon}'")
-    weapons = await armory.get_weapon_details(weapon)
-
-    logger.info(f"# of weapons found: {len(weapons)}")
-    result = weapons[0] # TODO: pagination
-
-    logger.info("Constructing weapon result")
-    DESCRIPTION = str(result.weapon_base_info) + "\n" + result.description
-    embed = discord.Embed(title=result.name, description= DESCRIPTION, color=constants.DISCORD_BG_HEX)
-    embed.set_thumbnail(url=result.icon)
-
-    for perk in result.weapon_perks:
-        if (perk.idx + 1) % 3 == 0:
-            embed.add_field(name="\u200b", value="\u200b", inline=True)
-        embed.add_field(name=perk.name, value=perk, inline=True)
-    embed.add_field(name="\u200b", value="\u200b", inline=True)
-
-    logger.info("Sending weapon result")
-    await ctx.send(embed=embed)
-
-@gunsmith.error
-async def on_error(ctx, error):
-    if hasattr(error, 'original'):
-        logger.exception(error.original)
-        if isinstance(error.original, ValueError):
-            logger.error(f"Command: {ctx.message.content}")
-            await ctx.send('Weapon could not be found.')
-            return
-        if isinstance(error.original, TypeError):
-            logger.error(f"Command: {ctx.message.content}")
-            logger.error('Failed to parse weapon')
-            await ctx.send('Failed to parse weapon. Please try again.')
-            return
-        if isinstance(error.original, discord.errors.HTTPException):
-            if error.original.status == 429:
-                logger.critical("Bot is rate-limited")
-            return
-        if isinstance(error.original, OperationalError):
-            logger.error(f"Command: {ctx.message.content}")
-            logger.error('Failed to find manifest')
-            await ctx.send('An error occured. Please try again.')
-            return
-    if isinstance(error, commands.BadArgument):
-        await ctx.send("Please enter the weapon name.")
-        return
-    if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send("Please enter the weapon name.")
-        return
 
 logger.info("Starting up bot")
 bot.add_cog(UpdateManifest(bot))
+bot.load_extension("cogs.weapons")
 bot.run(DISCORD_KEY)
 logger.info("Shutting down bot")
